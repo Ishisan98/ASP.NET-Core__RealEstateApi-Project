c4dad7833731397566e6d52d5b637b4b89e853b4
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-03-08 16:51:01 +0800

    GET Method

---
+++ RealEstateApi/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateApi.Models;
+
+namespace RealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private List<Category> categories = new List<Category>()
+        {
+            new Category () { Id = 0, Name = "Appartment", ImageUrl = "appartment.png"},
+            new Category () { Id = 1, Name = "Commercial", ImageUrl = "commercial.png"}
+        };
+
+
+        [HttpGet]
+        public IEnumerable <Category> get ()
+        {
+            return categories; 
+        } 
+    }
+}
