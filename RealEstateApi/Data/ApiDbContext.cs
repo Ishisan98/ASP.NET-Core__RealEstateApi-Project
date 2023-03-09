60a68eed73713682e288e6de9b8f3509b2b3267c
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-03-09 13:02:15 +0800

    Connected to Database and created a table

---
+++ RealEstateApi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateApi.Models;
+
+namespace RealEstateApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=ISHI;Database=RealEstateDb;integrated security=true; trustServerCertificate=true");
+        }
+    }
+}
