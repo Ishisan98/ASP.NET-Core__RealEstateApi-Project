5aa0ac74fd1b4dce28fff0a5e785581f067de795
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-03-09 15:39:43 +0800

    Added GET and POST methods

--- RealEstateApi/Controllers/CategoriesController.cs
+++ RealEstateApi/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstateApi.Data;
 using RealEstateApi.Models;
 
 namespace RealEstateApi.Controllers
@@ -8,35 +9,81 @@ namespace RealEstateApi.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        private static List<Category> categories = new List<Category>()
-        {
-            new Category () { Id = 0, Name = "Appartment", ImageUrl = "appartment.png"},
-            new Category () { Id = 1, Name = "Commercial", ImageUrl = "commercial.png"}
-        };
+        ApiDbContext _dbcontext = new ApiDbContext ();
 
-
-        [HttpGet]                               // api/categories
-        public IEnumerable <Category> get ()
+        [HttpGet]
+        public IEnumerable<Category> Get() 
         {
-            return categories; 
+            return _dbcontext.Categories;
+        }
+
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            var category = _dbcontext.Categories.FirstOrDefault(x => x.Id == id);
+            return category;
         }
 
         [HttpPost]
-        public void Post ([FromBody] Category category)
+        public void Post([FromBody] Category category)
         {
-            categories.Add (category);
+            _dbcontext.Categories.Add(category);
+            _dbcontext.SaveChanges();
         }
 
-        [HttpPut("{id}")]
-        public void Put (int id, [FromBody] Category category)
-        {
-            categories[id] = category;
-        }
 
-        [HttpDelete("{id}")]
-        public void Delete (int id)
-        {
-            categories.RemoveAt(id);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //private static List<Category> categories = new List<Category>()
+        //{
+        //    new Category () { Id = 0, Name = "Appartment", ImageUrl = "appartment.png"},
+        //    new Category () { Id = 1, Name = "Commercial", ImageUrl = "commercial.png"}
+        //};
+
+        //[HttpGet]  // api/categories
+        //public IEnumerable <Category> get ()
+        //{
+        //    return categories; 
+        //}
+
+        //[HttpPost]
+        //public void Post ([FromBody] Category category)
+        //{
+        //    categories.Add (category);
+        //}
+
+        //[HttpPut("{id}")]
+        //public void Put (int id, [FromBody] Category category)
+        //{
+        //    categories[id] = category;
+        //}
+
+        //[HttpDelete("{id}")]
+        //public void Delete (int id)
+        //{
+        //    categories.RemoveAt(id);
+        //}
+
+
     }
 }
