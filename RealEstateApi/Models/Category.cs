6ce7507652bd5cc5d040be9514ab65a564a6748a
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-03-10 01:18:23 +0800

    Added Content Negotiation and Model Validation

--- RealEstateApi/Models/Category.cs
+++ RealEstateApi/Models/Category.cs
@@ -1,12 +1,16 @@
-﻿namespace RealEstateApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateApi.Models
 {
     public class Category
     {
 
         public int Id { get; set; }
 
+        [Required (ErrorMessage = "Name cannot be empty")]
         public string Name { get; set; }
 
+        [Required (ErrorMessage = "ImageUrl cannot be empty")]
         public string ImageUrl { get; set; }
 
     }
